5e28c4bb010cc876a078d2bccd31da0a5adad606
Ethan Gill <ethan.gill@me.com>
2015-04-02 19:01:49 -0400

    merge with MazinZ

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <iomanip>
 #include <sstream>
+#include <stdlib.h>
+#include <unistd.h>
 using namespace std;
 
 struct Token{
@@ -38,6 +40,31 @@ private:
 	string token;
 	string usage;
 };
+
+struct Variable{
+	public:
+		Variable(string n, string v)
+		{
+			name = n;
+			value = v;	
+		}
+		string get_name()
+		{
+			return name;
+		}
+		string get_value() {
+			return value;
+		}
+		string set_value(string v){
+			value = v;
+		}
+	private:
+		name;
+		value;
+		
+	
+};
+vector<Variable> variableList;
 
 string typeGet(string token);
 vector<Token> scanner(string s);
@@ -294,6 +321,32 @@ bool parser(vector<Token> &scanned){
 	}
 	// no error found
 	return !founderror;
+	
+}
+
+void programRun(vector<Token> parsed){
+	if (upper(parsed[0]=="CD"){
+			chdir(parsed[1]);		
+	}
+	else if (upper(parsed[0])=="DEFPROMPT") {
+		promptToken = parsed[1];
+	}
+	else if (parsed[0].get_type()=="WORD" && parsed[1].get_token()=="="){
+		bool exists = false;
+		for (int i = 0; i < variableList.size(); i++){
+			if (parsed[0].get_token() == variableList[i].get_name()){
+				variableList[i].set_value() == parsed[2];
+				exists = true;
+				break;
+			}
+			if (!exists){
+				variableList.append(Variable(parsed[0],parsed[2]));
+			}
+		}
+	else if (parsed[0]=="BYE")
+		exit(0);
+	}
+	
 	
 }
 
